a74f5b54a96273d2dbeaa103f3b45890ab82fc4e
Pepe4ik <missenubr@gmail.com>
2025-10-17 12:32:15 +0100

    пока что так, снизу нормальный код, то что в комментах сверху это ну просто шизофазия, не знал как рекурсия работает прост

--- lesson_004/04_fractal.py
+++ lesson_004/04_fractal.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 import simple_draw as sd
+from simple_draw import COLOR_YELLOW
 
 # 1) Написать функцию draw_branches, которая должна рисовать две ветви дерева из начальной точки
 # Функция должна принимать параметры:
@@ -27,7 +28,48 @@ import simple_draw as sd
 # можно поиграть -шрифтами- цветами и углами отклонения
 
 # TODO здесь ваш код
+tochka = 0
+# def draw_branches():
+#     tochka_X = int(input("Введите точку X: "))
+#     tochka_Y = int(input("Введите точку Y: "))
+#     ugol = int(input("Введите угол: "))
+#     dlinna = int(input("Длинна ветвей: "))
+#
+#     vector = sd.get_vector((sd.get_point(tochka_X, tochka_Y)), ugol, dlinna, 10)
+#     vector.draw()
+#     for i in range (5):
+#         temp1 = vector.end_point.x
+#         temp2 = vector.end_point.y
+#         ugol += 30
+#         vector = sd.get_vector(sd.get_point(temp1,temp2), ugol, dlinna, 10)
+#         vector.draw()
+#
+#         ugol -= 60
+#         vector = sd.get_vector(sd.get_point(temp1,temp2), ugol, dlinna, 10)
+#         vector.draw()
+        #sd.circle(sd.get_point(temp1,temp2), 2, sd.COLOR_RED, 2)
+# КАРОЧЕ БРЕД ЯКИЙСЯ ПОЛУЧИВСЯ ЩАС БУДУ ДЕЛАТЬ НОРМАЛЬНО, Я ИЗУЧИВ ШО ТАКОЕ РЕКУРСИЯ
 
+
+def draw_branch(tochka, ugol, dlinna):
+    if dlinna < 10:
+        return
+    vector = sd.get_vector(tochka,ugol, dlinna, 2)
+    vector.draw()
+
+    tochka_2 = vector.end_point
+    draw_branch(tochka_2, ugol + 30, dlinna * 0.75)
+    draw_branch(tochka_2, ugol - 30, dlinna * 0.75)
+
+
+tochka_X = int(input("Введите точку X: "))
+tochka_Y = int(input("Введите точку Y: "))
+tochka = sd.get_point(tochka_X, tochka_Y)
+ugol = int(input("Введите угол: "))
+dlinna = int(input("Длинна ветвей: "))
+
+
+draw_branch(tochka, ugol, dlinna)
 # 4) Усложненное задание (делать по желанию)
 # - сделать рандомное отклонение угла ветвей в пределах 40% от 30-ти градусов
 # - сделать рандомное отклонение длины ветвей в пределах 20% от коэффициента 0.75
