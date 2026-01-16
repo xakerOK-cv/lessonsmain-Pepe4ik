ccd9b815f843cfb90f3549fe7e5d49e7c2d5e3b2
Pepe4ik <missenubr@gmail.com>
2026-01-16 13:05:24 +0000

    давняя домашка, чето сам не понял почему не сдал
    сейчас делаю седьмую домашку

--- lesson_007/01_snowfall.py
+++ lesson_007/01_snowfall.py
@@ -15,31 +15,3 @@ class Snowflake:
     # TODO здесь ваш код
 
 
-flake = Snowflake()
-
-while True:
-    flake.clear_previous_picture()
-    flake.move()
-    flake.draw()
-    if not flake.can_fall():
-        break
-    sd.sleep(0.1)
-    if sd.user_want_exit():
-        break
-
-# шаг 2: создать снегопад - список объектов Снежинка в отдельном списке, обработку примерно так:
-# flakes = get_flakes(count=N)  # создать список снежинок
-# while True:
-#     for flake in flakes:
-#         flake.clear_previous_picture()
-#         flake.move()
-#         flake.draw()
-#     fallen_flakes = get_fallen_flakes()  # подчитать сколько снежинок уже упало
-#     if fallen_flakes:
-#         append_flakes(count=fallen_flakes)  # добавить еще сверху
-#     sd.sleep(0.1)
-#     if sd.user_want_exit():
-#         break
-
-
-sd.pause()
